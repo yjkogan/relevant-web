0657ab9c3371c7490c3f63a2fe7563a777ec266b
Yonatan Kogan <yonatan@romotive.com>
2013-05-28 18:51:08 -0700

    Parameters are now parsed from the GET request (no longer hard coded!).
    Added input validation.

--- app/routes/root.js
+++ app/routes/root.js
@@ -1,5 +1,10 @@
 var app = require(APP_ROOT + '/app');
 
 app.get('/', function(req, res, next) {
-  return res.render('recommendedBook',{bookTitle: "The Great Gatsby",quote: "I like large parties", user: "yjkogan"})
+
+  if (!req.query.title) { return res.send("missing 'title' parameter in GET"); }
+  if (!req.query.quote) { return res.send("missing 'quote' parameter in GET"); }
+  if (!req.query.user) { return res.send("missing 'user' parameter in GET"); }
+
+  return res.render('recommendedBook',{bookTitle: req.query.title, quote: req.query.quote, user: req.query.user})
 });
